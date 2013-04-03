de683f4002f08c23e31798500c23779c497450eb
JFenlon <leeloo9>
2013-04-03 16:33:34 -0500

    Base
    Initial file list

---
+++ povgrid_UI_lib.js
@@ -0,0 +1,13 @@
+/*
+*************************************************************
+* POVGRID Custom UI function library
+* Created 04/03/2013
+*************************************************************
+*/
+
+// Main document read path
+$(document).ready(function () {
+
+	alert("hello");
+
+});
---
+++ povgrid_object_def.js
@@ -0,0 +1,297 @@
+/* This JS file defines all global objects and properties for all forms. */
+
+var segmentParams = Object.create(null)
+var vp1 = Object.create(null)
+var vp2 = Object.create(null)
+var vp3 = Object.create(null)
+var workspaceSettings = Object.create(null)
+var vpAttrs = Object.create(null)
+var vpStage = Object.create(null)
+var vpShape = Object.create(null)
+var lineWidth = .5;
+
+/* Object properties */
+
+/*
+*  Parameters for grid segments. This is used for gridlines
+*  anchored to verticle foreground lines only and passed to function
+*  to calculate grid points.
+*  Point1 is top anchor point on verticle line.
+*  Point2 is bottom anchor point on verticle line.
+*/
+Object.defineProperties(segmentParams, { 
+    staticPos:   { 
+        value:        0
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    }
+
+    , xIsStatic: {
+        value:        true
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    }
+
+    , point1:    { 
+        value:        0
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    }
+
+    , point2: { 
+        value:        0
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    }
+
+    , segmentCount: { 
+        value:        0
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    }            
+})
+
+/*
+*	workspaceSettings property
+*	orientation: 0 = landscape, 1 = portrait;
+*	persType: 1 = 1 point, 2 = 2 point, 3 = 3 point;
+*	isAutomatic: place and draw guides with assistance/assumptions based on perspective principles.
+*	hLineMidPoint: half-way point on horizon line.
+*/
+Object.defineProperties(workspaceSettings, { 
+    isLandscape:   { 
+        value:        1
+      , writable:     true
+      , configurable: false
+      , enumerable:   true 
+    }
+
+    , isAutomatic: {
+        value:        true
+      , writable:     true
+      , configurable: false
+      , enumerable:   true 
+    }
+
+    , paperColor: { 
+        value:        'white'
+      , writable:     true
+      , configurable: false
+      , enumerable:   true 
+    }
+
+    , persType: { 
+        value:        1
+      , writable:     true
+      , configurable: false
+      , enumerable:   true 
+    }
+
+    , hLineMidPoint: { 
+        value:        1
+      , writable:     true
+      , configurable: false
+      , enumerable:   true 
+    }   
+
+    , touchPadding: { 
+        value:        5
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    } 
+
+    , touchOpacity: { 
+        value:        .6
+      , writable:     true
+      , configurable: true
+      , enumerable:   true 
+    }  
+
+    , configLoaded: { 
+        value:        false
+      , writable:     true
+      , configurable: false
+      , enumerable:   true 
+    }            
+})
+
+Object.defineProperties(vp1,
+{
+    posX:
+    {
+          value:        0
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+
+    ,posY:
+    {
+          value:        0
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }  
+
+    ,id:
+    {
+          value:        'vp1'
+        , writable:     false
+        , configurable: false
+        , enumerable:   true 
+    }              
+})
+
+Object.defineProperties(vp2,
+{
+    posX:
+    {
+          value:        0
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+
+    ,posY:
+    {
+          value:        0
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }
+
+    ,id:
+    {
+          value:        'vp2'
+        , writable:     false
+        , configurable: false
+        , enumerable:   true 
+    }      
+})  
+
+Object.defineProperties(vp3,
+{
+    posX:
+    {
+          value:        0
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+
+    ,posY:
+    {
+          value:        0
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }
+
+    ,id:
+    {
+          value:        'vp3'
+        , writable:     false
+        , configurable: false
+        , enumerable:   true 
+    }      
+})  
+
+Object.defineProperties(vpAttrs,
+{
+    fillColor:
+    {
+          value:        'yellow'
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+
+    ,strokeColor:
+    {
+          value:        '#3D3D3D'
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }
+
+    ,radius:
+    {
+          value:        10
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }   
+
+    ,strokeWidth:
+    {
+          value:        1
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }  
+
+    ,opacity:
+    {
+          value:        1
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+})     
+
+
+/* This is the shape container element that will hold
+*  details about each shape. This container will be
+*  added to a collection of shape containers and maintained
+*  at all times.  
+*/  
+Object.defineProperties(vpShape,
+{
+    shapeID:
+    {
+          value:        'id'
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+
+    ,isTouchHandle:
+    {
+          value:        false
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }
+
+    ,isAnchoredtoVP:
+    {
+          value:        false
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }   
+
+    ,vpAnchorID:
+    {
+          value:        null
+        , writable:     true
+        , configurable: true
+        , enumerable:   true 
+    }  
+
+    ,shapeAnchorID:
+    {
+          value:        null
+        , writable:     true
+        , configurable: true
+        , enumerable:   true
+    }
+})   
+
+/* END PROPERTY DEFINITIONS */ 
